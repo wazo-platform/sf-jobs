43f0248422a412065bc6d8b92d78c7179b635a31
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-11-14 09:57:34 +0100

    acceptance: also upload configuration and commands output

---
+++ playbooks/wazo-acceptance/files/collect_commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOG_DIR=/var/log/commands
+mkdir -p ${LOG_DIR}
+
+log(){
+    local dest=${LOG_DIR}/$(echo "$*" | sed -e 's/ /_/g').txt
+    echo "Running '$* &$> $dest'"
+    $* &> $dest
+}
+
+log ip addr show
+log netstat -laputen
+log df
+log mount
+log systemctl -l show
+log systemctl -l
+log free -m
+
+for service in $(systemctl list-unit-files | awk '/\<enabled\>/{print $1}'); do
+    log journalctl -u $service
+done
